77a81f8b1a1b6886d5c6c6d147568bd22e9bdb87
MartinaC <martiinacanteros@gmail.com>
2024-11-05 19:35:36 -0300

    conexion al frontend

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,24 @@
-import express from 'express';
+
 import dotenv from 'dotenv';
 import dbconnect from './config/db';
 import routes from './routes/routes';
 
 dotenv.config();
 
-const PORT = parseInt(process.env.PORT ?? "3000", 10);
+const express = require('express');
+const cors = require('cors');
+
+const PORT = parseInt(process.env.PORT ?? "5000", 10);
 const HOST = process.env.HOST ?? "localhost";
 const app= express();
 
-app.use(express.json());
+app.use(cors(
+  {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type']
+  }
+));
 
 app.use('/api', routes);
 
